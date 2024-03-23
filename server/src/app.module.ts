5858211a417f63bbb5cd6dd24ef945d5fa7a7415
Sergiy <sergiy0378@gmail.com>
2024-03-23 11:04:30 +0200

    feat: edit main module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { TaskListsModule } from './task_lists/task_lists.module';
 
 @Module({
-  imports: [DatabaseModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    DatabaseModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TaskListsModule,
+  ],
   controllers: [],
   providers: [],
 })
