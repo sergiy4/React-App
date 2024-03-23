6de97ee8456655808f1d29e67f3e31cf7918a015
Sergiy <sergiy0378@gmail.com>
2024-03-23 12:41:42 +0200

    feat: add task module into main module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { TaskListsModule } from './task_lists/task_lists.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [
     DatabaseModule,
     ConfigModule.forRoot({ isGlobal: true }),
     TaskListsModule,
+    TasksModule,
   ],
   controllers: [],
   providers: [],
