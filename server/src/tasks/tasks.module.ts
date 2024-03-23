6e40e665f5fc602dcd23d29a929ac4f216cd5594
Sergiy <sergiy0378@gmail.com>
2024-03-23 18:03:25 +0200

    feat: add exports task list

--- server/src/tasks/tasks.module.ts
+++ server/src/tasks/tasks.module.ts
@@ -3,9 +3,10 @@ import { TasksService } from './services/tasks.service';
 import { TasksController } from './controllers/tasks.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Task } from './entities/task.entity';
+import { TaskListsModule } from 'src/task_lists/task_lists.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Task])],
+  imports: [TypeOrmModule.forFeature([Task]), TaskListsModule],
   controllers: [TasksController],
   providers: [TasksService],
 })
