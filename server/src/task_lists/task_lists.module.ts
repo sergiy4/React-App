8dc9c06e881c1a78dacb4f011ebd5f78bf4e2bc8
Sergiy <sergiy0378@gmail.com>
2024-03-25 08:41:12 +0200

    fix: fix tasks-list logic

--- server/src/task_lists/task_lists.module.ts
+++ server/src/task_lists/task_lists.module.ts
@@ -3,11 +3,12 @@ import { TaskListsController } from './controllers/task_lists.controller';
 import { TaskListsService } from './services/task_lists.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TaskList } from './entities/task_list.entity';
+import { TaskListsSubscriber } from './subscribers/task_lists.subscriber';
 
 @Module({
   imports: [TypeOrmModule.forFeature([TaskList])],
   controllers: [TaskListsController],
-  providers: [TaskListsService],
+  providers: [TaskListsService, TaskListsSubscriber],
   exports: [TaskListsService],
 })
 export class TaskListsModule {}
