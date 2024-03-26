306b9da8edf77d7ad58886f213e1a24ee3eea063
Sergiy <sergiy0378@gmail.com>
2024-03-26 11:42:51 +0200

    feat: provide store

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+
+import './index.css';
+import { Provider } from 'react-redux';
+import { store } from './libs/packages/store/store.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store.instance}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
