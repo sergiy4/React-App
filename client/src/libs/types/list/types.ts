05d1c952e2690e0b1ebe5fc8e61966240e2dcdee
Sergiy <sergiy0378@gmail.com>
2024-03-26 11:44:08 +0200

    feat: add lists types

---
+++ client/src/libs/types/list/types.ts
@@ -0,0 +1,5 @@
+export { type List } from './list.type';
+export { type GetListResponseDto } from './get-list.dto.type';
+export { type CreateListDto } from './create-list-request-dto.type';
+export { type UpdateListDto } from './update-list-request-dto.type';
+export { type DeleteListRequestDto } from './delete-list-request-dto.type';
